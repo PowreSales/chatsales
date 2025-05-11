1538f4a1788bf20f4968f7c63668950c839e4373
PowreSales <ezra.seidu74@gmail.com>
2025-05-11 17:03:52 +0000

    Update service-workers.js

--- service-workers.js
+++ service-workers.js
@@ -1,23 +1,13 @@
-const CACHE_NAME = 'pharmacy-sales-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/style.css',
-  '/manifest.json',
-  // List other necessary files or assets
-];
-
+// service-worker.js
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  console.log('Service Worker installed.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.');
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  // Default fetch handler
 });
